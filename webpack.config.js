167a0eb680ff92fc6de188caa4ff5577fefcb8aa
Glenn Murray <glenn.murray.bse@gmail.com>
2016-07-10 19:55:16 +1000

    Add chunk.
    TODO: Determine whether chunks are required in index.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   output: {
     path: __dirname + '/public',
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
     chunkFilename: '[id].bundle.js'
   },
   plugins: [
